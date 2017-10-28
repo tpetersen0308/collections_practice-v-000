8dcedae1d8d3a224dd302f40ef4c7b2f6e3d82e8
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-28 00:49:30 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,6 +17,11 @@ def swap_elements(array)
   array
 end
 
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
+
 def reverse_array(array)
   reversed_array = Array.new
   array.each { |element| reversed_array.unshift(element) }
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -30,6 +30,13 @@ describe 'collections practice' do
     end
   end
 
+  # Question 4.b
+  describe '#swap_elements_from_to' do
+    it 'swap the specified elements of an array' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"], 0, 2)).to eq(["scott", "ashley", "blake"])
+    end
+  end
+
   # Question 5
   describe '#reverse_array' do
     it 'reverse the order of an array of integers' do
